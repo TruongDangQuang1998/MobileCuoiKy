28b20e19803696a108c7497e69c5e8b017095888
Quang Truong <truongdangquang1998@gmail.com>
2021-06-20 11:19:03 +0700

    first commit

---
+++ app/src/main/java/com/example/a67_17069131_truongdangquang/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a67_17069131_truongdangquang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
